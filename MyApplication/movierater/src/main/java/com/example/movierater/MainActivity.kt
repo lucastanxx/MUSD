be06aefa1cac59949f4f3fe68281f98875031e77
LucasTanxx <172113P@mymail.nyp.edu.sg>
2018-11-05 12:49:49 +0800

    design almost done

---
+++ MyApplication/movierater/src/main/java/com/example/movierater/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.movierater
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun toastTheBread() {
+        println("Hello, world!")
+    }
+}
